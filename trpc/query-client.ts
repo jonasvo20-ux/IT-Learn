620bcd5a6331f8fe709d267233de9ef713295e95
Broodje56 <200642566+Broodje565@users.noreply.github.com>
2026-06-16 20:11:58 +0200

    Update query-client.ts

--- trpc/query-client.ts
+++ trpc/query-client.ts
@@ -3,20 +3,21 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import superjson from "superjson";
-
 export function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
         staleTime: 30 * 1000,
       },
-    },
-    dehydrate: {
-      serializeData: superjson.serialize,
-      shouldDehydrateQuery: defaultShouldDehydrateQuery,
-    },
-    hydrate: {
-      deserializeData: superjson.deserialize,
+      dehydrate: {
+        // serializeData: superjson.serialize,
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) ||
+          query.state.status === "pending",
+      },
+      hydrate: {
+        // deserializeData: superjson.deserialize,
+      },
     },
   });
 }
